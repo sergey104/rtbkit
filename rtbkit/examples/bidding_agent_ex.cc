ef5d99f8b7338a508331e47121964fd8bf3e8906
sergey104 <sfilperz@gmail.com>
2016-07-19 13:47:57 +0300

    test37

--- rtbkit/examples/bidding_agent_ex.cc
+++ rtbkit/examples/bidding_agent_ex.cc
@@ -110,11 +110,11 @@ struct FixedPriceBiddingAgent :
         config.creatives.push_back(Creative::sampleRR);
 
     //    config.providerConfig["appodeal"]["seat"] = 12;
-       // config.providerConfig["appodeal"]["iurl"] = "http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png";
+        config.providerConfig["appodeal"]["iurl"] = "http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png";
                 for(auto & c: config.creatives){
                     c.providerConfig["appodeal"]["adm"] = "some_adm";
                     c.providerConfig["appodeal"]["nurl"] = "http://52.208.150.10:17340?winprice=${AUCTION_PRICE}&auction_id=%{bidrequest.id}&adspot_id=%{creative.id}";
-                    c.providerConfig["appodeal"]["iurl"] = "http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png";
+         //           c.providerConfig["appodeal"]["iurl"] = "http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png";
                   }
 
         // Indicate to the router that we want our bid requests to be augmented
