6f66d623b71351706264c73381d57afa23a786a0
sergey104 <sfilperz@gmail.com>
2016-07-21 13:55:17 +0300

    new

--- rtbkit/examples/bidding_agent_ex.cc
+++ rtbkit/examples/bidding_agent_ex.cc
@@ -113,9 +113,9 @@ struct FixedPriceBiddingAgent :
         config.providerConfig["appodeal"]["iurl"] = "http://www.gnu.org";
 
                 for(auto & c: config.creatives){
-                    c.providerConfig["appodeal"]["adm"] = "some_adm";
-                    c.providerConfig["appodeal"]["nurl"] = "http://adserver.com/123456?winprice=${AUCTION_PRICE}&auction_id=%{bidrequest.id}&adspot_id=%{creative.id}";
-                    c.providerConfig["appodeal"]["iurl"] = "iurl_link";
+                    c.providerConfig["appodeal"]["adm"] = "<script src=\"mraid.js\"></script><a href=\"http://52.208.150.10:17341/click\" target=\"_blank\"><img width=\"300\" height=\"250\" style=\"border-style: none\" src=\"http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png\"/></a><script language=\"javascript\"type=\"text/javascript\">if(mraid.isViewable()){sendImpression();}else{mraid.addEventListener(\"viewableChange\",function(viewable){if(viewable){mraid.removeEventListener(\"viewableChange\",arguments.callee);sendImpression();}});}\nfunction sendImpression(){var urls=new Array();var i=0;urls[i++]=\"http://api.whatzbuzz.net/rtb/view?buzz=gAAAAABW4oe_ZnBIMcAliFv1T5ujGaBN_vCxv6BjFLq31DtNn_WDjuh-iF0QLyOITGXWllUaseOboW5xV3SswbIN5IstgO-vK-vh2S1MUb6hcnnYsaHEXCmZsv-zN8xYVM7jFRRWBLe4dYZYWRC9oi-5xgSWLbGBRt1ooldJ5rL-XK1-i_OT1QcMWJ-yCwpICSYSflv5mMGc&view=pixel.gif\";varhiddenSpan=document.createElement(\"span\");hiddenSpan.style.display=\"none\";var i=0;for(var i=0;i<urls.length;i++){var img=document.createElement(\"img\");img.src=urls[i];hiddenSpan.appendChild(img);}\nvar body=document.getElementsByTagName(\"body\")[0];body.appendChild(hiddenSpan);}</script>";
+                    c.providerConfig["appodeal"]["nurl"] = "http://52.208.150.10:17340/123456?winprice=${AUCTION_PRICE}&auction_id=d2a3a24d-7c0b-4805-9317-19f1783ceb101468775636296&adspot_id=0";
+                    c.providerConfig["appodeal"]["iurl"] = "http://amadoad-dev.eu-west-1.elasticbeanstalk.com/_banners/a4/75/a4757c5908c8ed6805d23dd44c8d8098b2f7b28e.png";
                   }
 
         // Indicate to the router that we want our bid requests to be augmented
