d534bc15a90944bb4841d26b174e8a09c2a8b72e
sergey104 <sfilperz@gmail.com>
2016-09-19 14:08:00 +0300

    some corrections4

--- rtbkit/examples/bidding_agent_ag.cc
+++ rtbkit/examples/bidding_agent_ag.cc
@@ -224,7 +224,7 @@ struct FixedPriceBiddingAgent :
 
             // Config parameter sent used by the augmentor to determine which
             // tag to set.
-            augConfig.config = Json::Value(5000);
+            augConfig.config = Json::Value(5);
 
             // Instruct to router to filter out all bid requests who have not
             // been tagged by our frequency cap augmentor.
