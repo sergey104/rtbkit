a4f824c92d2a5e0cbdccaad927d6cf19987a3d7e
sergey104 <sfilperz@gmail.com>
2016-09-21 08:53:26 +0300

    final for two

--- launch.sh
+++ launch.sh
@@ -12,7 +12,7 @@ tmux new-window -d -t rtb:6 -n 'augmentor' 'tail -F ./logs/augmentor.log'
 tmux new-window -d -t rtb:7 -n 'router' 'tail -F ./logs/router.log'
 tmux new-window -d -t rtb:8 -n 'post-auction' 'tail -F ./logs/post-auction.log'
 tmux new-window -d -t rtb:9 -n 'fixed-price-agent' 'tail -F ./logs/fixed-price-agent.log'
-tmux new-window -d -t rtb:0 -n 'fixed-price-agent1' 'tail -F ./logs/fixed-price-agent1.log'
+tmux new-window -d -t rtb:10 -n 'fixed-price-agent1' 'tail -F ./logs/fixed-price-agent1.log'
 if [[ "$*" != *--quiet* ]]
 then
     tmux attach -t rtb
